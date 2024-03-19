4e26ad1e84c57b3ea52e30ad31c0d5e37f4db08b
Charan <charankulal0241@gmail.com>
2024-03-19 20:16:29 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include 'assets/php/boilerplate.php'
+?>
+
+<div class="text-center">
+<h1 class="text-danger heading">Welcome to YOUR Bank</h1>
+</div>
+
+<div class="box col-md-8 offset-md-2">
+
+<div >
+    <img src="assets/img/register-bank.png" alt="Register Account Image" class="index-img"/>
+</div>
+
+<div class="row text-center">
+<div class="card emp-login-card" style="width: 17rem; margin-left: 30px;">
+ 
+  <div class="card-body ">
+    <h5 class="card-title">Employee Login</h5>
+    <p class="card-text">For authorized personnel use only</p>
+    <a href="#" class="btn btn-danger">Login</a>
+  </div>
+</div>
+<div class="card mx-2" style="width: 17rem;">
+  
+  <div class="card-body">
+    <h5 class="card-title">Customer Login</h5>
+    <p class="card-text">Already registered customer click here!!</p>
+    <a href="#" class="btn btn-primary">Login</a>
+  </div>
+</div>
+<div class="card mx-2" style="width: 17rem;">
+  
+  <div class="card-body">
+    <h5 class="card-title">Customer Register</h5>
+    <p class="card-text">Are you a new user? Create account!!</p>
+    <a href="customer_registration.php" class="btn btn-secondary">Register</a>
+  </div>
+</div>
+</div>
+</div>
+
